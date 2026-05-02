04fb7cc957eab59fb4350d9644ca071cbcee20d5
Brmmrn <brahim.merouane1998@gmail.com>
2026-05-02 15:35:17 +0200

    Update Deck.c

--- Deck.c
+++ Deck.c
@@ -13,10 +13,10 @@ void deck_init(Deck *paquet) {
         int nb_exemplaires;
  if (valeur_carte <= 1) {
     nb_exemplaires = 1;
-} else {
+}   else {
     nb_exemplaires = valeur_carte;
 }
-for (int exemplaire = 0; exemplaire < nb_exemplaires; exemplaire++) {
+   for (int exemplaire = 0; exemplaire < nb_exemplaires; exemplaire++) {
             paquet->cartes[paquet->taille].type   = NUM;
             paquet->cartes[paquet->taille].valeur = valeur_carte;
             paquet->taille++;
@@ -53,4 +53,36 @@ void deck_shuffle(Deck *paquet) {
 
 
 //Deck draw retourn et retourn au dessus//
+Carte deck_draw(Deck *paquet) {
+    Carte carte_vide = { NUM, -1 };
+
+        if (paquet->taille == 0) {
+        return carte_vide;
+    }
+
+    paquet->taille--;
+      return paquet->cartes[paquet->taille];
+}
+
+
+//Deck print affiche les cartes restantes//
+void deck_print(const Deck *paquet) {
+    printf(" Paquet (%d cartes restantes) \n", paquet->taille);
+
+    for (int position = 0; position < paquet->taille; position++) {
+        const Carte *carte_courante = &paquet->cartes[position];
+
+           if (carte_courante->type == NUM) {
+            printf("  [%2d] NUM        valeur = %d\n",
+                   position, carte_courante->valeur);
+        }    else if (carte_courante->type == BONUS_MULT) {
+            printf("  [%2d] BONUS_MULT valeur = x %d\n",
+                   position, carte_courante->valeur);
+        }     else if (carte_courante->type == BONUS_ADD) {
+            printf("  [%2d] BONUS_ADD  valeur = + %d\n",
+                   position, carte_courante->valeur);
+        }
+    }
+}
+
 
