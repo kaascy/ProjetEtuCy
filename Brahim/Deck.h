710b3bbfd683d00299b840523face6d775cf98dd
Brmmrn <brahim.merouane1998@gmail.com>
2026-05-12 20:23:15 +0200

    Create Deck.h

---
+++ Brahim/Deck.h
@@ -0,0 +1,45 @@
+#ifndef DECK_H
+#define DECK_H
+
+#define DECK_MAX_SIZE 85
+
+typedef enum {
+NUM,
+BONUS_MULT,
+BONUS_ADD
+} CardType;
+
+typedef struct {
+CardType type;
+int      value;
+} Card;
+
+typedef struct {
+Card cards[DECK_MAX_SIZE];
+int  size;
+} Deck;
+
+typedef struct {
+char name[50];
+Card hand[20];
+int  hand_size;
+int  round_score;
+int  total_score;
+int  bonus_mult;
+int  bonus_add;
+} Player;
+
+typedef enum {
+ROUND_CONTINUE,
+ROUND_LOST,
+ROUND_WIN
+} RoundStatus;
+
+void deck_init(Deck *deck);
+void deck_shuffle(Deck *deck);
+Card deck_draw(Deck *deck);
+void deck_print(const Deck *deck);
+int  deck_is_empty(const Deck *deck);
+int  deck_size(const Deck *deck);
+
+#endif
